8b0c30ce1fa86e05cb39f6037dcf0fa4b2833da1
gruuuuuuuu <gru20021104@gmail.com>
2022-05-27 15:32:28 +0800

    Guess GUI game

---
+++ src/main/java/UI/GuessFrame.java
@@ -0,0 +1,41 @@
+package UI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GuessFrame extends JFrame {
+    //Fields
+    JButton button= new JButton("HI");
+    JLabel label = new JLabel("Zzzz...");
+    //Constructors
+    //super 呼叫副類別的程式碼
+    public GuessFrame(){
+        super();
+        setSize(600,400);
+        setLocation(400,300);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+              //  System.out.println("Hello!");
+                label.setText("Hello!");
+            }
+        });
+//        若下面這行沒一道上面，則上面無法打出label
+//        JLabel label = new JLabel("Zzzz...");
+        setLayout(new FlowLayout());
+        add(button);
+        add(label);
+        setVisible(true);
+    }
+    //Methods
+
+
+    public static void main(String[] args) {
+        GuessFrame guessFrame = new GuessFrame();
+//        guessFrame.setSize(600,500);
+//        guessFrame.setVisible(true);
+    }
+}
---
+++ src/main/java/UI/MyForm.java
@@ -0,0 +1,10 @@
+package UI;
+
+import javax.swing.*;
+
+public class MyForm {
+    private JButton iMGruButton;
+    private JPanel panel1;
+    private JTextField textField1;
+    private JCheckBox checkBox1;
+}
