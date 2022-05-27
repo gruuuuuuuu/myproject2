e5599163b652560eb2efc316c00aa870e969c358
gruuuuuuuu <gru20021104@gmail.com>
2022-05-27 16:10:41 +0800

    Homework 1

--- src/main/java/UI/GuessFrame.java
+++ src/main/java/UI/GuessFrame.java
@@ -9,6 +9,7 @@ public class GuessFrame extends JFrame {
     //Fields
     JButton button= new JButton("HI");
     JLabel label = new JLabel("Zzzz...");
+    JTextField number = new JTextField(8);
     //Constructors
     //super 呼叫副類別的程式碼
     public GuessFrame(){
@@ -19,13 +20,16 @@ public class GuessFrame extends JFrame {
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
+                int num =Integer.parseInt(number.getText());
+                System.out.println(num);
               //  System.out.println("Hello!");
-                label.setText("Hello!");
+//                label.setText("Hello!");
             }
         });
 //        若下面這行沒一道上面，則上面無法打出label
 //        JLabel label = new JLabel("Zzzz...");
         setLayout(new FlowLayout());
+        add(number)
         add(button);
         add(label);
         setVisible(true);
